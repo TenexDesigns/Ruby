a2e25bc384c374f97c02b7c197e1f0be5b7d39c1
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-05-28 11:37:19 +0300

    Create Comments.py

---
+++ Comments.py
@@ -0,0 +1,204 @@
+Certainly! In Ruby, comments are used to add explanatory notes or remarks within the code that are ignored by the interpreter.
+Heres an explanation of comments in Ruby with code samples:
+  
+  
+  
+
+Single-Line Comments:
+
+Single-line comments start with the # symbol and extend until the end of the line.
+Example:
+
+# This is a single-line comment.
+puts "Hello, World!" # This line prints a greeting.
+
+
+
+
+Multi-Line Comments:
+
+Multi-line comments are typically used for longer comments or comment blocks. 
+They begin with =begin and end with =end delimiters.
+Example:
+
+=begin
+This is a
+multi-line comment.
+It can span multiple lines.
+=end
+
+
+
+
+Inline Comments:
+
+Inline comments are used to provide clarifications on the same line of code.
+They are often placed after the code.
+Example:
+
+x = 5  # Assigning the value 5 to variable x.
+Comments are helpful for documenting your code, making notes, or temporarily disabling code blocks. 
+They improve code readability and can assist other developers in understanding your intentions.
+
+Its good practice to use comments sparingly and provide meaningful explanations.
+Keep in mind that comments should be kept up-to-date with the code and should not state the obvious.
+Well-commented code can facilitate collaboration and maintenance of the codebase.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+...
