d020ae0fc10d29413ce645235fdf2064748dd305
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-10-19 14:13:20 +0300

    Create 1_IRB (Interactive Ruby).java

---
+++ IRB (Interactive Ruby)/1_IRB (Interactive Ruby).java
@@ -0,0 +1,56 @@
+IRB, or Interactive Ruby, is a tool that allows you to interact with Ruby in a read-eval-print loop (REPL). This means that you can type Ruby code into IRB and see the results immediately.
+
+IRB can be used in both Ruby and Rails applications. In Ruby, it is often used for development and testing. For example, you can use IRB to experiment with new code snippets or to debug existing code. In Rails, IRB can be used to access the Rails console. This is a special IRB session that has access to all of your Rails models and controllers.
+
+To start IRB in Ruby, simply type `irb` in a terminal window. To start IRB in Rails, type `rails console`.
+
+Here is a simple example of how to use IRB:
+
+```ruby
+# Start IRB
+irb
+
+# Print the value of 1 + 1
+puts 1 + 1
+
+# Exit IRB
+exit
+```
+
+Output:
+
+```
+2
+```
+
+You can also use IRB to define and call methods. For example, the following code defines a method called `greet` and then calls it:
+
+```ruby
+# Define a method called greet
+def greet(name)
+  puts "Hello, #{name}!"
+end
+
+# Call the greet method
+greet("Bard")
+```
+
+Output:
+
+```
+Hello, Bard!
+```
+
+IRB is a powerful tool that can be used for a variety of tasks. It is especially useful for development and testing.
+
+Here are some additional tips for using IRB:
+
+* You can use the `tab` key to complete code snippets. This can save you time and help to prevent errors.
+* You can use the `up` and `down` arrow keys to navigate through your command history. This can be useful if you need to edit a previous command.
+* You can use the `help` command to get help with IRB commands. For example, to get help with the `puts` command, you would type `help puts`.
+
+I hope this helps!
+
+
+
+  .....
