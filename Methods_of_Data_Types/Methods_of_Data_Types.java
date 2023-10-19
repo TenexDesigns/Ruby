77961d5e9118fc66e14d730e8c9347192c8be24d
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-10-19 15:47:23 +0300

    Create Methods_of_Data_Types.java

---
+++ Methods_of_Data_Types/Methods_of_Data_Types.java
@@ -0,0 +1,58 @@
+Here are some of the most common methods for data types in Ruby:
+
+**Arrays**
+
+* `length`: Returns the number of elements in the array.
+* `first`: Returns the first element in the array.
+* `last`: Returns the last element in the array.
+* `at(index)`: Returns the element at the specified index in the array.
+* `push(object)`: Adds an element to the end of the array.
+* `pop`: Removes the last element from the array and returns it.
+* `shift`: Removes the first element from the array and returns it.
+* `unshift(object)`: Adds an element to the beginning of the array.
+* `include?(object)`: Returns `true` if the array contains the specified object, and `false` otherwise.
+* `index(object)`: Returns the index of the specified object in the array, or `nil` if the object is not found.
+* `sort`: Sorts the elements of the array in ascending order.
+* `reverse`: Reverses the order of the elements in the array.
+
+**Hashes**
+
+* `length`: Returns the number of key-value pairs in the hash.
+* `keys`: Returns an array of all the keys in the hash.
+* `values`: Returns an array of all the values in the hash.
+* `[](key)`: Returns the value associated with the specified key in the hash, or `nil` if the key does not exist.
+* `[]=(key, value)`: Sets the value associated with the specified key in the hash.
+* `delete(key)`: Removes the key-value pair associated with the specified key from the hash, and returns the value, or `nil` if the key does not exist.
+* `has_key?(key)`: Returns `true` if the hash contains the specified key, and `false` otherwise.
+* `each`: Iterates over the key-value pairs in the hash, and yields the key and value to the block.
+
+**Strings**
+
+* `length`: Returns the number of characters in the string.
+* `empty?`: Returns `true` if the string is empty, and `false` otherwise.
+* `upcase`: Returns a copy of the string with all lowercase characters converted to uppercase.
+* `downcase`: Returns a copy of the string with all uppercase characters converted to lowercase.
+* `strip`: Returns a copy of the string with all leading and trailing whitespace removed.
+* `include?(substring)`: Returns `true` if the string contains the specified substring, and `false` otherwise.
+* `index(substring)`: Returns the index of the specified substring in the string, or `nil` if the substring is not found.
+* `split(separator)`: Splits the string into an array of strings, using the specified separator.
+* `join(separator)`: Joins the elements of an array of strings into a string, using the specified separator.
+
+**Numbers**
+
+* `+`: Performs addition.
+* `-`: Performs subtraction.
+* `*`: Performs multiplication.
+* `/`: Performs division.
+* `%`: Performs modulo division (returns the remainder of the division operation).
+* `abs`: Returns the absolute value of the number.
+* `round(precision)`: Rounds the number to the specified precision.
+* `ceil`: Rounds the number up to the nearest integer.
+* `floor`: Rounds the number down to the nearest integer.
+
+These are just a few of the many methods available for data types in Ruby. For more information, please consult the Ruby documentation.
+
+
+
+
+  ......
