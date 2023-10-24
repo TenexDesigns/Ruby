8c7e4e0c7be4cd4c62655755dd888334325039f3
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-10-24 17:27:41 +0300

    Create 1_Safe navigation operator_(&.).java

---
+++ Safe navigation operator_(&.)/1_Safe navigation operator_(&.).java
@@ -0,0 +1,52 @@
+The safe navigation operator `&.` in Ruby is a useful feature introduced in Ruby 2.3. It allows you to safely call a method on an object without raising a `NoMethodError` if the object is `nil`. This operator is often used to handle situations where you may have optional or potentially missing objects, such as when dealing with nested hashes or objects.
+
+Here's how the safe navigation operator works:
+
+- When you use `&.` before calling a method on an object, Ruby will only call the method if the object is not `nil`. If the object is `nil`, the method call is simply ignored, and `nil` is returned.
+
+- It provides a convenient way to write concise and safe code without having to write explicit conditional checks for `nil?`.
+
+Here are some scenarios where you might want to use the safe navigation operator:
+
+1. **Accessing Hash Keys:**
+   When you want to access a key in a hash, but you're not sure if the key exists, you can use the safe navigation operator to prevent a `NoMethodError`.
+
+   ```ruby
+   person = { name: 'John', address: { city: 'New York' } }
+   city = person[:address]&.fetch(:city, 'Unknown')
+   ```
+
+2. **Calling Methods on Optional Objects:**
+   When you have an object that might be `nil`, and you want to call a method on it without explicitly checking for `nil`.
+
+   ```ruby
+   user = find_user_by_id(42)
+   username = user&.username
+   ```
+
+3. **Chaining Method Calls:**
+   You can chain method calls using the safe navigation operator, making it safe to traverse a chain of objects.
+
+   ```ruby
+   result = obj&.method1&.method2&.method3
+   ```
+
+Here's an example of using the safe navigation operator in Ruby:
+
+```ruby
+person = { name: 'John', address: nil }
+
+# Without safe navigation
+city = person[:address].fetch(:city, 'Unknown')  # Raises NoMethodError
+
+# With safe navigation
+city = person[:address]&.fetch(:city, 'Unknown')  # No error, returns 'Unknown'
+```
+
+In this example, the safe navigation operator prevents a `NoMethodError` when trying to fetch the `:city` key from a `nil` object.
+
+The safe navigation operator is a handy feature to simplify your code and make it more robust when dealing with potentially missing objects or keys, improving readability and maintainability.
+
+
+
+  ....
